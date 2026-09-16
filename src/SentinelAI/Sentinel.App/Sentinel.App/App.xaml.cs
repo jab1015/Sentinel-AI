35c45452329be61c7d9d361aeda0c81aeeeb0d08
jab1015 <decideforusapp@gmail.com>
2026-09-16 14:36:38 -0400

    Fix startup diagnostics task namespace

--- src/SentinelAI/Sentinel.App/Sentinel.App/App.xaml.cs
+++ src/SentinelAI/Sentinel.App/Sentinel.App/App.xaml.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
 using Windows.Storage;
 
